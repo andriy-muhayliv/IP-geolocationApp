bbb0e150553005c32ba9b498d764d5b0682db989
gyd3ukc <70544293+gyd3ukc@users.noreply.github.com>
2021-08-17 17:46:31 +0300

    Add files via upload

--- src/DataConponent.jsx
+++ src/DataConponent.jsx
@@ -6,31 +6,31 @@ const DataComponent = (props) => {
 
     return(
         <div className={style.ip__data}>
-            <p>{props.ipData.ip}</p>
-            <p>{props.ipData.version}</p>
-            <p>{props.ipData.city}</p>
-            <p>{props.ipData.region}</p>
-            <p>{props.ipData.region_code}</p>
-            <p>{props.ipData.country_code}</p>
-            <p>{props.ipData.country_code_iso3}</p>
-            <p>{props.ipData.country_name}</p>
-            <p>{props.ipData.country_capital}</p>
-            <p>{props.ipData.country_tld}</p>
-            <p>{props.ipData.continent_code}</p>
-            <p>{props.ipData.in_eu}</p>
-            <p>{props.ipData.postal}</p>
-            <p>{props.ipData.latitude}</p>
-            <p>{props.ipData.longitude}</p>
-            <p>{props.ipData.timezone}</p>
-            <p>{props.ipData.utc_offset}</p>
-            <p>{props.ipData.country_calling_code}</p>
-            <p>{props.ipData.currency}</p>
-            <p>{props.ipData.currency_name}</p>
-            <p>{props.ipData.languages}</p>
-            <p>{props.ipData.country_area}</p>
-            <p>{props.ipData.country_population}</p>
-            <p>{props.ipData.asn}</p>
-            <p>{props.ipData.org}</p>
+            <p><span>IP:</span> {props.ipData.ip}</p>
+            <p><span>VERSION:</span> {props.ipData.version}</p>
+            <p><span>CITY:</span> {props.ipData.city}</p>
+            <p><span>REGION:</span> {props.ipData.region}</p>
+            <p><span>REGIONT CODE:</span> {props.ipData.region_code}</p>
+            <p><span>COUNTRY CODE:</span> {props.ipData.country_code}</p>
+            <p><span>ISO:</span> {props.ipData.country_code_iso3}</p>
+            <p><span>COUNTRY:</span> {props.ipData.country_name}</p>
+            <p><span>COUNTRY CAPITAL:</span>{props.ipData.country_capital}</p>
+            <p><span>LTD:</span> {props.ipData.country_tld}</p>
+            <p><span>CODE:</span> {props.ipData.continent_code}</p>
+            <p><span>IN EU:</span> {props.ipData.in_eu}</p>
+            <p><span>POSTAL:</span> {props.ipData.postal}</p>
+            <p><span>LATITUDE:</span> {props.ipData.latitude}</p>
+            <p><span>LONGITUDE:</span> {props.ipData.longitude}</p>
+            <p><span>TIMEZONE:</span> {props.ipData.timezone}</p>
+            <p><span>UTC:</span> {props.ipData.utc_offset}</p>
+            <p><span>CALL CODE:</span> {props.ipData.country_calling_code}</p>
+            <p><span>CURRENCY:</span> {props.ipData.currency}</p>
+            <p><span>CURRENCY NAME:</span> {props.ipData.currency_name}</p>
+            <p><span>LANGUAGE:</span> {props.ipData.languages}</p>
+            <p><span>COUNTRY AREA:</span> {props.ipData.country_area}</p>
+            <p><span>COUNTRY POPYLATION:</span> {props.ipData.country_population}</p>
+            <p><span>ASN:</span> {props.ipData.asn}</p>
+            <p><span>ORG:</span> {props.ipData.org}</p>
             
         </div>
     )
